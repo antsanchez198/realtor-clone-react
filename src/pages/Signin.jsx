6112a68625f3298c58164e4f26593f8c683bf5f2
Anthony Sanchez <antsanchez198@gmail.com>
2024-01-23 23:06:31 -0500

    Sign in page started

--- src/pages/Signin.jsx
+++ src/pages/Signin.jsx
@@ -1,8 +1,21 @@
-import React from 'react'
-
 const Signin = () => {
   return (
-    <div>Signin</div>
+    <section>
+      <h1 className="text-3xl text-center mt-6 font-bold">Sign In</h1>
+
+      <div>
+        <div className="md:w-[67%] lg:w-[50%] mb-12 md:mb-6">
+          <img src="https://images.unsplash.com/flagged/photo-1564767609342-620cb19b2357?q=80&w=773&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" 
+          alt="key"/>
+        </div>
+
+        <div>
+          <form>
+            <input></input>
+          </form>
+        </div>
+      </div>
+    </section>
   )
 }
 
