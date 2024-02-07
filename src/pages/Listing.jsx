0a57f6da6154e2f2699a6256bbd8020f87b5a04a
Anthony Sanchez <antsanchez198@gmail.com>
2024-02-07 14:29:39 -0500

    Share Link Fucntionality

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -13,7 +13,8 @@ import 'swiper/css';
 import 'swiper/css/navigation';
 
 // import required modules
-import { EffectFade, Navigation, Autoplay } from 'swiper/modules';
+import { EffectFade, Navigation, Autoplay, Pagination } from 'swiper/modules';
+import {FaShare} from 'react-icons/fa'
 
 
 
@@ -22,6 +23,7 @@ const Listing = () => {
     const params = useParams();
     const [listing, setListing] = useState(null)
     const [loading, setLoading] = useState(true)
+    const [shareLinkedCopied, setShareLinkedCopied] = useState(false)
 
     useEffect(() => {
         const fetchListing = async () => {
@@ -42,8 +44,19 @@ const Listing = () => {
 
     return (
         <main>
-            <Swiper slidesPerView={1} navigation={true} modules={[Navigation, EffectFade, Autoplay]} pagination={{ type: "progressbar" }}
-                effect='fade' autoplay={{ delay: 3000 }}>
+            <Swiper
+                spaceBetween={30}
+                centeredSlides={true}
+                autoplay={{
+                    delay: 3000,
+                    disableOnInteraction: false,
+                }}
+                pagination={{
+                    clickable: true,
+                }}
+                navigation={true}
+                modules={[Autoplay, Pagination, Navigation]}
+            >
                 {listing.imgUrls.map((url, index) =>
                     <SwiperSlide>
                         <div
@@ -54,9 +67,25 @@ const Listing = () => {
                             }}>
 
                         </div>
+                        {/* <div className='relative w-full overflow-hidden h-[300px]'>
+                            <img src={`${listing.imgUrls[index]}`} className='w-full bg-cover bg-no-repeat bg-center' />
+                        </div> */}
                     </SwiperSlide>
                 )}
             </Swiper>
+            <div className="fixed top-[13%] right-[3%] z-10 bg-white cursor-pointer border-2 border-gray-200 rounded-full w-12 h-12 flex justify-center items-center"
+                onClick={() => {
+                    navigator.clipboard.writeText(window.location.href)
+                    setShareLinkedCopied(true)
+                    setTimeout(() => {
+                        setShareLinkedCopied(false)
+                    }, 2000)
+                }}>
+                <FaShare className='text-lg text-slate-500' />
+            </div>
+            {shareLinkedCopied && (
+                <p className='fixed top-[23%] right-[5%] font-semibold border-2 border-gray-400 rounded-md bg-white z-10 p-2'>Link Copied</p>
+            )}
         </main>
     )
 }
