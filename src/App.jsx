c9c258c80fc6c9e7534288c10578101f0d88a689
Anthony Sanchez <antsanchez198@gmail.com>
2024-02-11 15:15:00 -0500

    Create the places for rent and sale pages

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ import 'react-toastify/dist/ReactToastify.css';
 import CreateListing from './pages/CreateListing'
 import EditListing from './pages/EditListing'
 import Listing from './pages/Listing'
+import Category from './pages/Category'
 
 function App() {
 
@@ -27,6 +28,7 @@ function App() {
             <Route path="/profile" element={<Profile />}></Route>
           </Route>
           <Route path="/offers" element={<Offers />}></Route>
+          <Route path="/category/:categoryName" element={<Category />}></Route>
           <Route path="/category/:categoryName/:listingId" element={<Listing />}></Route>
           <Route path="/sign-in" element={<Signin />}></Route>
           <Route path="/sign-up" element={<Signup />}></Route>
