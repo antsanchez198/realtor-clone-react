234a470bfd74195062708ceea32a4b8522366b6b
Anthony Sanchez <antsanchez198@gmail.com>
2024-02-13 14:53:01 -0500

    Fixed logo and app title

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -32,7 +32,7 @@ const Home = () => {
   }, [])
 
   //Places for rent
-  const [rentListing, setRentListing] = useState(null);
+  const [rentListings, setRentListings] = useState(null);
 
   useEffect(() => {
     const fetchListings = async () => {
@@ -47,7 +47,7 @@ const Home = () => {
             data: doc.data(),
           })
         })
-        setRentListing(listings);
+        setRentListings(listings);
       } catch (error) {
         console.log(error)
       }
@@ -56,7 +56,7 @@ const Home = () => {
   }, [])
 
   //Places for sale
-  const [saleLisitng, setSaleListing] = useState(null);
+  const [saleListings, setSaleListings] = useState(null);
 
   useEffect(() => {
     const fetchListings = async () => {
@@ -71,7 +71,7 @@ const Home = () => {
             data: doc.data(),
           })
         })
-        setSaleListing(listings);
+        setSaleListings(listings);
       } catch (error) {
         console.log(error)
       }
@@ -82,53 +82,66 @@ const Home = () => {
   return (
     <div>
       <Slider />
-      <div className="m-w-6xl mx-auto pt-4 space-y6">
+      <div className="max-w-6xl mx-auto pt-4 space-y-6">
         {offerListings && offerListings.length > 0 && (
           <div className="m-2 mb-6">
-            <h2 className="px-3 text-2xl mt-6 font-semibold ">Recent Offers</h2>
+            <h2 className="px-3 text-2xl mt-6 font-semibold">Recent offers</h2>
             <Link to="/offers">
-              <p className="px-3 text-sm text-blue-600
-              hover:text-blue-800 transition duration-150 ease-in-out">Show more offers</p>
-              <ul className="sm:gird sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-                {offerListings.map((listing) => (
-                  <ListingItem
-                    key={listing.id}
-                    listing={listing.data} />
-                ))}
-              </ul>
+              <p className="px-3 text-sm text-blue-600 hover:text-blue-800 transition duration-150 ease-in-out">
+                Show more offers
+              </p>
             </Link>
+            <ul className="sm:grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 ">
+              {offerListings.map((listing) => (
+                <ListingItem
+                  key={listing.id}
+                  listing={listing.data}
+                  id={listing.id}
+                />
+              ))}
+            </ul>
           </div>
         )}
-        {rentListing && rentListing.length > 0 && (
+        {rentListings && rentListings.length > 0 && (
           <div className="m-2 mb-6">
-            <h2 className="px-3 text-2xl mt-6 font-semibold ">Places for Rent</h2>
+            <h2 className="px-3 text-2xl mt-6 font-semibold">
+              Places for rent
+            </h2>
             <Link to="/category/rent">
-              <p className="px-3 text-sm text-blue-600
-              hover:text-blue-800 transition duration-150 ease-in-out">Show more places for rent</p>
-              <ul className="sm:gird sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-                {rentListing.map((listing) => (
-                  <ListingItem
-                    key={listing.id}
-                    listing={listing.data} />
-                ))}
-              </ul>
+              <p className="px-3 text-sm text-blue-600 hover:text-blue-800 transition duration-150 ease-in-out">
+                Show more places for rent
+              </p>
             </Link>
+            <ul className="sm:grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 ">
+              {rentListings.map((listing) => (
+                <ListingItem
+                  key={listing.id}
+                  listing={listing.data}
+                  id={listing.id}
+                />
+              ))}
+            </ul>
           </div>
         )}
-        {saleLisitng && saleLisitng.length > 0 && (
-          <div className="m-2 mb-6">
-            <h2 className="px-3 text-2xl mt-6 font-semibold ">Places for Sale</h2>
+        {saleListings && saleListings.length > 0 && (
+          <div className="m-2">
+            <h2 className="px-3 text-2xl mt-6 font-semibold">
+              Places for sale
+            </h2>
             <Link to="/category/sale">
-              <p className="px-3 text-sm text-blue-600
-              hover:text-blue-800 transition duration-150 ease-in-out">Show more places for sale</p>
-              <ul className="sm:gird sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-                {saleLisitng.map((listing) => (
-                  <ListingItem
-                    key={listing.id}
-                    listing={listing.data} />
-                ))}
-              </ul>
+              <p className="px-3 text-sm text-blue-600 hover:text-blue-800 transition duration-150 ease-in-out">
+                Show more places for sale
+              </p>
             </Link>
+            <ul className="sm:grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 ">
+              {saleListings.map((listing) => (
+                <ListingItem
+                  key={listing.id}
+                  listing={listing.data}
+                  id={listing.id}
+                />
+              ))}
+            </ul>
           </div>
         )}
       </div>
